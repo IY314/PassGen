9a0b259820343f75cf4843ffabd98ab14475bebc
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-01-24 18:23:50 -0800

    Add clipboard copying option

---
+++ Makefile
@@ -0,0 +1,4 @@
+venv:
+	python3 -m venv venv
+	pip install -U pip
+	pip install -r requirements.txt
--- passgen.py
+++ passgen.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import pyperclip
 import secrets
 
 
@@ -9,15 +10,26 @@ def main():
     words = get_data(args)
     indices = generate_indices(args, words)
     passphrase = get_passphrase(args, words, indices)
-    print(passphrase)
+    if args.output is None or args.output == 'clipboard':
+        pyperclip.copy(passphrase)
+    elif args.output == 'print':
+        print(passphrase)
+    else:
+        print(f'Unknown output option {args.output}')
 
 
 def generate_args():
     parser = argparse.ArgumentParser(prog='PassGen')
-    parser.add_argument('--adjectives', '-a', type=int, help='The number of adjectives (default 2)')
-    parser.add_argument('--nouns', '-N', type=int, help='The number of nouns (default 1)')
-    parser.add_argument('--builtins', '-b', action='store_true', help='Use only builtin words')
-    parser.add_argument('--number', '-n', action='store_true', help='Add number at end')
+    parser.add_argument('--adjectives', '-a', type=int, help='The number of '
+                        'adjectives (default 2)')
+    parser.add_argument('--nouns', '-N', type=int, help='The number of nouns '
+                        '(default 1)')
+    parser.add_argument('--builtins', '-b', action='store_true', help='Use '
+                        'only builtin words')
+    parser.add_argument('--number', '-n', action='store_true', help='Add '
+                        'number at end')
+    parser.add_argument('--output', '-o', help='The form that the passphrase '
+                        'is displayed (default clipboard)')
     return parser.parse_args()
 
 
@@ -26,12 +38,12 @@ def get_data(args):
     words = {'adjectives': [], 'nouns': []}
     if not args.builtins:
         paths.append('./words/extension')
-    
+
     for dirpath in paths:
         for path in os.listdir(dirpath):
             with open(f'{dirpath}/{path}') as f:
                 words.update(json.load(f))
-    
+
     return words
 
 
@@ -44,7 +56,7 @@ def generate_indices(args, words):
         for _2 in range(args.adjectives or 2):
             adj_idx = secrets.randbelow(len_adj)
             indices.append((adj_idx, 'adjectives'))
-        
+
         noun_idx = secrets.randbelow(len_noun)
         indices.append((noun_idx, 'nouns'))
 
@@ -60,11 +72,11 @@ def get_passphrase(args, words, indices):
             raise Exception(f'Unknown location {location!r}')
         word = word_list[index].capitalize()
         result += word
-    
+
     if args.number:
         number = secrets.randbits(7)
         result += str(number)
-    
+
     return result
 
 
